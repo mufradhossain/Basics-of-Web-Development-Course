faf1a3b970166910d968b9bc7234198249a476ad
mufradhossain <sheikh15-2773@diu.edu.bd>
2023-05-19 21:39:57 +0600

    Create assignment.js

---
+++ module 3/assignment.js
@@ -0,0 +1,17 @@
+function multiplicationTable(num) {
+  for (var i = 1; i <= 10; i++) {
+    console.log(num + "x" + i + "=" + num * i);
+  }
+}
+
+//testing function
+//Table for 1
+multiplicationTable(1);
+//Table for 2
+multiplicationTable(2);
+//Table for 3
+multiplicationTable(3);
+//Table for 4
+multiplicationTable(4);
+//Table for 5
+multiplicationTable(5);
